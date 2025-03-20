5d466054564dc30cb5e914590d50f93d9fdf2947
leonardo <leonardo>
2025-03-20 13:00:41 -0300

    authenticacao de rota privada somente usuario logado finish

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -15,9 +15,13 @@ export function Header() {
               Tarefas<span>+</span>
             </h1>
           </Link>
-          <Link href="/dashboard" className={styles.link}>
-            Meu Painel
-          </Link>
+
+          { session?.user && (
+            <Link href="/dashboard" className={styles.link}>
+              Meu Painel
+            </Link>
+          )}
+          
         </nav>
         {status === "loading" ? (
           <></>
--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,5 +1,9 @@
+import { GetServerSideProps } from 'next'
 import styles from './styles.module.css'
 import Head from 'next/head'
+
+import { getSession } from 'next-auth/react';
+
 export default function Dashboard(){
     return(
         <div className={styles.container}>
@@ -9,4 +13,22 @@ export default function Dashboard(){
             <h1>Pagina Painel</h1>
         </div>
     )
+}
+
+export const getServerSideProps: GetServerSideProps = async ({req}) =>{
+    const session = await getSession({ req });
+    // console.log(session)
+
+    if(!session?.user){
+        // se não tem usuario vai pra home
+        return{
+            redirect:{
+                destination:"/",
+                permanent:false,
+            }
+        }
+    }
+    return{
+        props:{},
+    }
 }
