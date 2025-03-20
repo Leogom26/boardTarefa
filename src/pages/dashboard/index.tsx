29eba9411561188748d1cd517592b01eab166f5f
leonardo <leonardo>
2025-03-20 13:41:53 -0300

    painel

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -3,42 +3,49 @@ import styles from './styles.module.css'
 import Head from 'next/head'
 
 import { getSession } from 'next-auth/react';
+import { Textarea } from '@/components/textarea';
 
 export default function Dashboard() {
-    return (
-        <div className={styles.container}>
-            <Head>
-                <title>Meu painel de tarefas</title>
-            </Head>
+	return (
+		<div className={styles.container}>
+			<Head>
+					<title>Meu painel de tarefas</title>
+			</Head>
 
-            <main className={styles.main}>
-                <section className={styles.content}>
-                    <div className={styles.contentForm}>
-                        {/* Adicione o conteúdo desejado aqui */}
-                    </div>
-                </section>
-            </main>
+			<main className={styles.main}>
+				<section className={styles.content}>
+					<div className={styles.contentForm}>
+						<h1 className={styles.title}>Qual sua tarefa?</h1>
+						<form>
+							<Textarea/>
+							<div className={styles.checkboxArea}>
+								
+							</div>
+						</form>
+					</div>
+				</section>
+			</main>
 
-        </div>
-    )
+		</div>
+	)
 }
 
 export const getServerSideProps: GetServerSideProps = async ({ req }) => {
     const session = await getSession({ req });
     // console.log(session)
 
-    if (!session?.user) {
-        // se não tem usuario vai pra home
-        return {
-            redirect: {
-                destination: "/",
-                permanent: false,
-            }
-        }
-    }
+	if (!session?.user) {
+		// se não tem usuario vai pra home
+		return {
+				redirect: {
+						destination: "/",
+						permanent: false,
+				}
+		}
+	}
 
-    return {
-        props: {},
-    }
+	return {
+		props: {},
+	}
 }
 
