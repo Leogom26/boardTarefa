cc5e0d439870b6c0e75a9eb777f6105cd0616e0c
leonardo <leonardo>
2025-03-20 13:41:21 -0300

    construindo pagina painel

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -4,31 +4,41 @@ import Head from 'next/head'
 
 import { getSession } from 'next-auth/react';
 
-export default function Dashboard(){
-    return(
+export default function Dashboard() {
+    return (
         <div className={styles.container}>
             <Head>
                 <title>Meu painel de tarefas</title>
             </Head>
-            <h1>Pagina Painel</h1>
+
+            <main className={styles.main}>
+                <section className={styles.content}>
+                    <div className={styles.contentForm}>
+                        {/* Adicione o conteúdo desejado aqui */}
+                    </div>
+                </section>
+            </main>
+
         </div>
     )
 }
 
-export const getServerSideProps: GetServerSideProps = async ({req}) =>{
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
     const session = await getSession({ req });
     // console.log(session)
 
-    if(!session?.user){
+    if (!session?.user) {
         // se não tem usuario vai pra home
-        return{
-            redirect:{
-                destination:"/",
-                permanent:false,
+        return {
+            redirect: {
+                destination: "/",
+                permanent: false,
             }
         }
     }
-    return{
-        props:{},
+
+    return {
+        props: {},
     }
 }
+
