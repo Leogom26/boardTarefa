0ad4ebb49fa2b1ba025ef6d03a71c4cc1197978f
leonardo <leonardo>
2025-03-20 10:04:47 -0300

    projeto estruturado npm run dev

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,15 @@
 import Head from "next/head";
-import styles from "@/styles/Home.module.css";
+import styles from "@/styles/home.module.css";
 
 export default function Home() {
   return (
     <>
+    <div className={styles.container}>
       <Head>
         <title>Tarefas+ | Organize suas tarefas de forma fácil</title>
       </Head>
       <h1> Meu projeto </h1>
+    </div>
     </>
   );
 }
